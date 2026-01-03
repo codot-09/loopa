26b8488705b840239c8cd10d84bf9f6d2d3aaa11
codot-09 <onabiyev626@gmail.com>
2026-01-03 13:39:29 +0500

    caching and scheduling enabled

--- src/main/java/com/example/loopa/LoopaApplication.java
+++ src/main/java/com/example/loopa/LoopaApplication.java
@@ -2,8 +2,12 @@ package com.example.loopa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableCaching
+@EnableScheduling
 public class LoopaApplication {
 
 	public static void main(String[] args) {
