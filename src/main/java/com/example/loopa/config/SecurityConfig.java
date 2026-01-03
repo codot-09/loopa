31204e2014c91e1dcc082705e21a59e91c409ec8
codot-09 <onabiyev626@gmail.com>
2026-01-03 08:59:49 +0500

    admin panel cors

--- src/main/java/com/example/loopa/config/SecurityConfig.java
+++ src/main/java/com/example/loopa/config/SecurityConfig.java
@@ -47,7 +47,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOriginPatterns(List.of("https://loopa.buzz","https://api.loopa.buzz"));
+        configuration.setAllowedOriginPatterns(List.of("https://loopa.buzz","https://api.loopa.buzz","https://admin.loopa.buzz"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
