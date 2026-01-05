dd24b1ec876b16be63aafb5838cb60ee1959809a
codot-09 <onabiyev626@gmail.com>
2026-01-05 22:29:13 +0500

    product search bug fix

--- src/main/java/com/example/loopa/config/BotConfig.java
+++ src/main/java/com/example/loopa/config/BotConfig.java
@@ -1,19 +1,19 @@
-//package com.example.loopa.config;
-//
-//import com.example.loopa.service.bot.BotService;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.telegram.telegrambots.meta.TelegramBotsApi;
-//import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-//import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
-//
-//@Configuration
-//public class BotConfig {
-//
-//    @Bean
-//    public TelegramBotsApi telegramBotsApi(BotService botService) throws TelegramApiException {
-//        TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
-//        api.registerBot(botService);
-//        return api;
-//    }
-//}
+package com.example.loopa.config;
+
+import com.example.loopa.service.bot.BotService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class BotConfig {
+
+    @Bean
+    public TelegramBotsApi telegramBotsApi(BotService botService) throws TelegramApiException {
+        TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
+        api.registerBot(botService);
+        return api;
+    }
+}
